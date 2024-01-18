9bf620b44795c089e7da3628f947b5229f313d14
yeguu <qsc130@gmail.com>
2024-01-18 20:19:14 +0900

    #6.7_Form_Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -33,19 +33,49 @@ import { useForm } from "react-hook-form";
 // };
 
 const ToDoList = () => {
-  const { register, watch } = useForm();
-  // console.log(watch());
+  // useForm 훅을 사용하여 폼 상태 관리
+  const { register, handleSubmit, formState } = useForm();
+
+  // form이 유효할 때 호출하는 콜백 함수
+  const onValid = (data: any) => {
+    console.log("data", data);
+  };
+
+  console.log("error", formState.errors);
 
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("firstName")} placeholder="First Name" />
-        <input {...register("lastName")} placeholder="Last Name" />
-        <input {...register("username")} placeholder="Username" />
-        <input {...register("password")} placeholder="Password" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        // handleSubmit: onValid를 호출하기 전에 입력값을 유효성 검사함
+        onSubmit={handleSubmit(onValid)}
+      >
+        {/* 
+        필수 입력 필드에 대한 유효성 검사 및 기타 표준 HTML 유효성 규칙을 포함함
+        각 field는 registration process에서 key로 사용될 "이름"을 가져야 함
+        */}
+        <input {...register("email", { required: true })} placeholder="Email" />
         <input
-          {...register("passwordConfirmation")}
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("passwordConfirmation", {
+            required: "Password is required",
+            minLength: { value: 5, message: "Your password is too short." },
+          })}
           placeholder="Password Confirmation"
         />
         <button>Add</button>
@@ -78,4 +108,7 @@ input {...register('lastName', { required: true })}
   - input의 변화 구독
   - 지정된 input을 감시하고 해당 값을 반환
   - input 값을 렌더링하고 조건에 따라 무엇을 렌더링할지 결정하는 데 유용
+
+- handleSubmit: ((data: Object, e?: Event) => void, (errors: Object, e?: Event) => void) => Function
+  - form 유효성 검사가 성공하면 form 데이터를 받음
 */

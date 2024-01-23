6e53c41b5ddd5653bc1133a370d83cfa874c2da8
yeguu <qsc130@gmail.com>
2024-01-23 18:48:26 +0900

    #6.15_Immutability_part_Two

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -4,17 +4,18 @@ import { IToDo, toDoState } from "../atoms";
 const ToDo = ({ text, category, id }: IToDo) => {
   const setToDos = useSetRecoilState(toDoState);
 
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const {
-      currentTarget: { name },
-    } = event;
-
+  const onClick = (newCategory: IToDo["category"]) => {
     setToDos((oldToDos) => {
       // 1) target 경로 찾기
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
       // 2) 새 category로 새로운 to do 만들기
-      const newToDo = { text, id, category: name };
-      return oldToDos;
+      const newToDo = { text, id, category: newCategory };
+      // 3) category update
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
     });
   };
 
@@ -22,17 +23,17 @@ const ToDo = ({ text, category, id }: IToDo) => {
     <li>
       <span>{text}</span>
       {category !== "DOING" && (
-        <button name="DOING" onClick={onClick}>
+        <button name="DOING" onClick={() => onClick("DOING")}>
           Doing
         </button>
       )}
       {category !== "TO_DO" && (
-        <button name="TO_DO" onClick={onClick}>
+        <button name="TO_DO" onClick={() => onClick("TO_DO")}>
           To Do
         </button>
       )}
       {category !== "DONE" && (
-        <button name="DONE" onClick={onClick}>
+        <button name="DONE" onClick={() => onClick("DONE")}>
           Done
         </button>
       )}
